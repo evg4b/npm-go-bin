7501a4c196b9a96bdb15eeaf4a49c0be53f264c4
Yauheni Abramovich <yauheni.abramovich@appcast.io>
2023-10-14 01:22:18 -0300

    Fixed types

--- src/helpres/streams.ts
+++ src/helpres/streams.ts
@@ -3,20 +3,21 @@ import { extract as extractTar } from 'tar';
 import { ParseOne as extractZip } from 'unzipper';
 import { createWriteStream } from 'fs';
 import { join } from 'path';
+import { Readable } from 'stream';
 import { finished } from 'stream/promises';
 
-const processZip = (stream: ReadableStream, binName: string, path: string): WritableStream =>
+const processZip = (stream: Readable, binName: string, path: string): WritableStream =>
   stream.pipe(extractZip(new RegExp(binName), { forceStream: true }))
     .pipe(createWriteStream(join(path, binName)));
 
-const processTarGz = (stream: ReadableStream, binName: string, path: string): WritableStream =>
+const processTarGz = (stream: Readable, binName: string, path: string): WritableStream =>
   stream.pipe(createGunzip())
     .pipe(extractTar({ cwd: path }, [binName]));
 
-const processBin = (stream: ReadableStream, binName: string, path: string): WritableStream =>
+const processBin = (stream: Readable, binName: string, path: string): WritableStream =>
   stream.pipe(createWriteStream(join(path, binName)));
 
-export const unpackArchive = (stream: ReadableStream, { url, binName }: PackageInfo, path: string): Promise<void> => {
+export const unpackArchive = (stream: Readable, { url, binName }: PackageInfo, path: string): Promise<void> => {
   const { pathname } = new URL(url);
 
   if (pathname.endsWith('.tar.gz')) {
